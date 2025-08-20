d491270a27aa9902af956a14584074ad6bdec949
Muhammad-Muzzamal <ashrafmuzzamal7@gmail.com>
2025-08-19 23:12:10 -0700

    Day 16 of web development

---
+++ Day16/index.js
@@ -0,0 +1,14 @@
+let num = 12;
+
+if (num % 3 === 0 && num + 1 === 15 && num - 1 == 11) {
+  console.log("Safe");
+} else {
+  console.log("Unsafe");
+}
+
+let str = "abc";
+if (str.length === 3 && str[0] === "a") {
+  console.log("Good String\n");
+} else {
+  console.log("Bad String");
+}
