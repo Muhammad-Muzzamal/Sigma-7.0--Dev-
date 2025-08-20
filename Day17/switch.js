9d9c47c5bd04489b169938af9e0ac46a65fb5076
Muhammad-Muzzamal <ashrafmuzzamal7@gmail.com>
2025-08-19 23:45:42 -0700

    day 177

---
+++ Day17/switch.js
@@ -0,0 +1,45 @@
+let color = "green";
+
+switch (color) {
+  case "red":
+    console.log("Stop the car\n");
+    break;
+  case "yellow":
+    console.log("Ready to go\n");
+    break;
+  case "green":
+    console.log("You can go\n");
+    break;
+  default:
+    console.log("Break the signal");
+    break;
+}
+let day = 3;
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+  case 3:
+    console.log(`Wednesday\n`);
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Invalid day");
+    break;
+}
+
+
+alter
