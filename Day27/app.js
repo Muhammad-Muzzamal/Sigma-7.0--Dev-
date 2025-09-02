ef8f31b67ebcdb3beff1d0ebf234151c455ae7b3
Muhammad-Muzzamal <ashrafmuzzamal7@gmail.com>
2025-09-01 18:08:26 -0700

    add today practice

---
+++ Day27/app.js
@@ -0,0 +1,5 @@
+let mainHeading = document.querySelector("#mainHeading");
+let mainBody = document.querySelector('.mainBody');
+
+mainHeading.innerText = "Hello World";
+mainBody.innerHTML = "<b>I am in the main body</b><br> Hello World Kya hal ha hai";
